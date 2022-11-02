7c5c602779fbda33ba54f4637f1088d53d68a4fc
Zed Chamaa <sales@nineprovince.com>
2022-11-02 23:33:28 +0100

    Serve different size images based on window width

--- js/main.js
+++ js/main.js
@@ -12,27 +12,75 @@ http.onload = function() {
     let media = JSON.parse(this.responseText);
     let output = "";
 
+    const windowWidth = window.innerWidth;
+
     for (let item of media) {
-      output += `
-      <div class="media-card-general">
-      <div class="thumbnail">
-        <img src="${item.thumbnail.regular.small}" alt="${item.title}" />
-        <div class="badge icon-bookmark badge__bookmark-empty"></div>
+      if (windowWidth >= 1024) {
+        output += `
+        <div class="media-card-general">
+        <div class="thumbnail">
+          <img src="${item.thumbnail.regular.large}" alt="${item.title}" />
+          <div class="badge icon-bookmark badge__bookmark-empty"></div>
+        </div>
+        <ul class="list list--inner">
+          <li>${item.year}</li>
+          <li>&middot;</li>
+          <li>
+            <img class="icon--small" src="/assets/icon-category-movie.svg" alt="${item.category}" />
+            ${item.category}
+          </li>
+          <li>&middot;</li>
+          <li>${item.rating}</li>
+        </ul>
+        <h3>${item.title}</h3>
+        </div>
       </div>
-      <ul class="list list--inner">
-        <li>${item.year}</li>
-        <li>&middot;</li>
-        <li>
-          <img class="icon--small" src="/assets/icon-category-movie.svg" alt="${item.category}" />
-          ${item.category}
-        </li>
-        <li>&middot;</li>
-        <li>${item.rating}</li>
-      </ul>
-      <h3>${item.title}</h3>
+        `;
+      }
+      else if (windowWidth >= 768) {
+        output += `
+        <div class="media-card-general">
+        <div class="thumbnail">
+          <img src="${item.thumbnail.regular.medium}" alt="${item.title}" />
+          <div class="badge icon-bookmark badge__bookmark-empty"></div>
+        </div>
+        <ul class="list list--inner">
+          <li>${item.year}</li>
+          <li>&middot;</li>
+          <li>
+            <img class="icon--small" src="/assets/icon-category-movie.svg" alt="${item.category}" />
+            ${item.category}
+          </li>
+          <li>&middot;</li>
+          <li>${item.rating}</li>
+        </ul>
+        <h3>${item.title}</h3>
+        </div>
       </div>
-    </div>
-      `;
+        `;
+      }
+      else {
+        output += `
+        <div class="media-card-general">
+        <div class="thumbnail">
+          <img src="${item.thumbnail.regular.small}" alt="${item.title}" />
+          <div class="badge icon-bookmark badge__bookmark-empty"></div>
+        </div>
+        <ul class="list list--inner">
+          <li>${item.year}</li>
+          <li>&middot;</li>
+          <li>
+            <img class="icon--small" src="/assets/icon-category-movie.svg" alt="${item.category}" />
+            ${item.category}
+          </li>
+          <li>&middot;</li>
+          <li>${item.rating}</li>
+        </ul>
+        <h3>${item.title}</h3>
+        </div>
+      </div>
+        `;
+      }
     }
     document.querySelector('.grid').innerHTML = output;
   }
