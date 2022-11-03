ef4dce099c0c3fc08104a5591a5c4eb293343358
Zed Chamaa <sales@nineprovince.com>
2022-11-03 14:58:26 +0100

    Show relevant media category icon

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ http.onload = function() {
 
     for (let item of media) {
       let imageSize;
+      let categoryIcon;
+      const movieIcon = '/assets/icon-category-movie.svg';
+      const tvSeriesIcon = '/assets/icon-category-tv.svg';
 
       if (windowWidth >= 1024) {
         imageSize = item.thumbnail.regular.large;
@@ -27,6 +30,9 @@ http.onload = function() {
         imageSize = item.thumbnail.regular.small;
       }
 
+      if (item.category === 'Movie') categoryIcon = movieIcon; 
+      if (item.category === 'TV Series') categoryIcon = tvSeriesIcon; 
+
       output += `
         <div class="media-card-general">
         <div class="thumbnail">
@@ -37,7 +43,7 @@ http.onload = function() {
           <li>${item.year}</li>
           <li>&middot;</li>
           <li>
-            <img class="icon--small" src="/assets/icon-category-movie.svg" alt="${item.category}" />
+            <img class="icon--small" src="${categoryIcon}" alt="${item.category}" />
             ${item.category}
           </li>
           <li>&middot;</li>
