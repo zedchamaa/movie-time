058fb185e4c8302b474b7af1b0363815d822c269
Zed Chamaa <sales@nineprovince.com>
2022-11-11 16:44:36 +0100

    Backup latest working code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 /* Strict Mode */
 "use strict";
 
-const url = './data/data.json';
+const url = "./data/data.json";
 
 function getData() {
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    outData(data);
-  })
+    .then((response) => response.json())
+    .then((data) => {
+      outData(data);
+    });
 }
 
 getData();
@@ -16,6 +16,7 @@ getData();
 function outData(val) {
   populateRecommendedItems(val);
   populateTrendingItems(val);
+  bookmarkTrendingMedia(val);
 }
 
 function populateRecommendedItems(val) {
@@ -25,21 +26,19 @@ function populateRecommendedItems(val) {
   for (let item of val) {
     let imageSize;
     let categoryIcon;
-    const movieIcon = '/assets/icon-category-movie.svg';
-    const tvSeriesIcon = '/assets/icon-category-tv.svg';
+    const movieIcon = "/assets/icon-category-movie.svg";
+    const tvSeriesIcon = "/assets/icon-category-tv.svg";
 
     if (windowWidth >= 1024) {
       imageSize = item.thumbnail.regular.large;
-    } 
-    else if (windowWidth >= 768) {
+    } else if (windowWidth >= 768) {
       imageSize = item.thumbnail.regular.medium;
-    }
-    else {
+    } else {
       imageSize = item.thumbnail.regular.small;
     }
 
-    if (item.category === 'Movie') categoryIcon = movieIcon; 
-    if (item.category === 'TV Series') categoryIcon = tvSeriesIcon; 
+    if (item.category === "Movie") categoryIcon = movieIcon;
+    if (item.category === "TV Series") categoryIcon = tvSeriesIcon;
 
     output += `
       <div class="media-card-general">
@@ -62,7 +61,7 @@ function populateRecommendedItems(val) {
     </div>
     `;
   }
-  document.querySelector('.recommended').innerHTML = output;
+  document.querySelector(".recommended").innerHTML = output;
 }
 
 function populateTrendingItems(val) {
@@ -73,18 +72,17 @@ function populateTrendingItems(val) {
     if (item.isTrending) {
       let imageSize;
       let categoryIcon;
-      const movieIcon = '/assets/icon-category-movie.svg';
-      const tvSeriesIcon = '/assets/icon-category-tv.svg';
+      const movieIcon = "/assets/icon-category-movie.svg";
+      const tvSeriesIcon = "/assets/icon-category-tv.svg";
 
       if (windowWidth >= 1024) {
         imageSize = item.thumbnail.trending.large;
-      } 
-      else {
+      } else {
         imageSize = item.thumbnail.trending.small;
       }
 
-      if (item.category === 'Movie') categoryIcon = movieIcon; 
-      if (item.category === 'TV Series') categoryIcon = tvSeriesIcon; 
+      if (item.category === "Movie") categoryIcon = movieIcon;
+      if (item.category === "TV Series") categoryIcon = tvSeriesIcon;
 
       output += `
       <div class="box">
@@ -109,24 +107,39 @@ function populateTrendingItems(val) {
       `;
     }
   }
-  document.querySelector('.media-scroller').innerHTML = output;
-  bookmarkTrendingMedia();
+  document.querySelector(".media-scroller").innerHTML = output;
 }
 
 // Create a function to bookmark the trending media
 
-function bookmarkTrendingMedia() {
+function bookmarkTrendingMedia(val) {
+  document.querySelector(".media-scroller").addEventListener(
+    "click",
+    (e) => {
+      const btn = e.target.closest("button.icon-bookmark");
+      if (!btn) return;
+      // toggle bookmark badge icon
+      btn.classList.toggle("badge__bookmark-full");
 
-  document.querySelector(".media-scroller").addEventListener("click", e => {
-    const btn = e.target.closest('button.icon-bookmark');
-    if(!btn) return;
-    // toggle bookmark badge icon
-    btn.classList.toggle('badge__bookmark-full');
-    // change bookmarked status
+      // find which media item was clicked
+      const mediaTitleParent = btn.parentNode.parentNode;
+      const mediaTitle = mediaTitleParent.children[2].textContent;
 
+      // change bookmarked status
+      for (let item of val) {
+        // TODO: continue from here, still unable to modify the json file properties
 
-
-  }, {passive: true});
-
-
+        if (mediaTitle === item.title) {
+          if (item.isBookmarked) {
+            item.isBookmarked = false;
+            console.log(val); // TODO: delete
+          } else {
+            item.isBookmarked = true;
+            console.log(val); // TODO: delete
+          }
+        }
+      }
+    },
+    { passive: true }
+  );
 }
